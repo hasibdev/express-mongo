82856069930e90a412556c70c3f5c7021e56be14
hasibdev <79460308+hasibdev@users.noreply.github.com>
2022-01-26 01:55:38 +0600

    mongo db connected

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import { app } from './config/express'
+import mongoose from './config/mongoose'
 import { port } from './config/vars.js'
 
 const http = require('http')
+
+// open mongoose connection
+mongoose.connect()
 
 // Server Running
 const server = http.createServer(app)
