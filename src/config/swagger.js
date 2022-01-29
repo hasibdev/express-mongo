013bcbc3e39374bc600c5425df31a33afe53fb29
hasibdev <79460308+hasibdev@users.noreply.github.com>
2022-01-29 11:24:59 +0600

    updated

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -6,9 +6,21 @@ const options = {
       openapi: '3.0.0',
       info: {
          title: 'Api Documentation',
+         description: 'This is a api Documentation for my application.',
          version: '1.0.0',
+         contact: {
+            email: 'hasib.webdev@gmail.com'
+         },
+         liscence: {
+            name: 'Apache 2.0',
+            url: 'http://apache.org/'
+         }
       },
-      servers: [{ url: 'http://localhost:8000' }]
+      servers: [
+         { url: 'http://localhost:8000', description: 'local server' },
+         { url: 'http://example.com', description: 'Production server' },
+      ],
+
    },
    apis: ['src/apidocs/*.yaml'], // files containing annotations as above
 }
